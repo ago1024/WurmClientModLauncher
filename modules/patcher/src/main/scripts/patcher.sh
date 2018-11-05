1476399bd987612052da8a5a22d08aba0c0c3c52
Alexander Gottwald <alexander.gottwald@gmail.com>
2018-11-05 22:42:49 +0100

    Fix linux java version

--- modules/patcher/src/main/scripts/patcher.sh
+++ modules/patcher/src/main/scripts/patcher.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 java() {
-	../runtime/jre1.8.0_121/bin/java $*
+	../runtime/jre1.8.0_172/bin/java $*
 }
 java -classpath ./patcher.jar:./javassist.jar org.gotti.wurmunlimited.patcher.PatchClientJar
